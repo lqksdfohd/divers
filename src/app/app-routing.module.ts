fa3f70a19cdb30648ad2ab278b9b57a35336af70
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-09-19 17:49:18 +0200

    feat: créer le composant calculatrice
    créer le composant
    créer la route vers le composant
    commencer le style du template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculatriceComponent } from './calculatrice/calculatrice.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'calculatrice', component:CalculatriceComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
