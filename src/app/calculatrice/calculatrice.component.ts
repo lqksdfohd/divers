904746c12a364bb61bf0578e40334ffbfb86fb1a
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-09-25 11:42:39 +0200

    test calcul avec stack

--- src/app/calculatrice/calculatrice.component.ts
+++ src/app/calculatrice/calculatrice.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { fromEvent, map } from 'rxjs';
 
 @Component({
   selector: 'app-calculatrice',
@@ -6,5 +7,85 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculatrice.component.css']
 })
 export class CalculatriceComponent {
+  calcul: string[];
+  resultat: string;
+  affichageCalcul: string;
 
+  constructor() {
+    this.calcul = [];
+    this.resultat = "";
+    this.affichageCalcul = "";
+  }
+
+  push(input: string): void {
+    const regexNombre = /[0-9]+/;
+    //si l'array calcul est vide et que l'input est un chiffre on l'ajoute à l'array.
+    if (this.calcul.length == 0 && regexNombre.test(input)) {
+      this.calcul.push(input);
+    } else {
+      let last = this.calcul.pop() as string;
+      //si le dernier element de calcul est un nombre et que l'input est un chiffre on les concatene.
+      if (regexNombre.test(last) && regexNombre.test(input)) {
+        last += input;
+        this.calcul.push(last);
+      }
+      //si le dernier element de calcul est une opération et que l'input est aussi une opération 
+      //on remplace l'ancienne operation;
+      else if (!regexNombre.test(last) && !regexNombre.test(input)) {
+        this.calcul.push(input);
+      }//si le dernier element de calcul est un nombre et que l'input est une opération
+      //ou si c'est l'inverse on push les éléments dans l'ordre.
+      else {
+        this.calcul.push(last);
+        this.calcul.push(input);
+      }
+    }
+    this.formatterCalcul();
+  }
+
+  calculer(): void {
+    let calculTemp:string[] = this.calculPrioritaire();
+    this.resultat = calculTemp.join(" ");
+    //this.calculSecondaire(calculTemp);
+  }
+
+  calculPrioritaire():string[] {
+    const work: string[] = this.calcul.slice();
+    if (this.checkCalcul()) {
+      while (work.length > 1) {
+        let n1 = Number.parseInt(work.pop() as string);
+        let op = work.pop() as string;
+        let n2 = Number.parseInt(work.pop() as string);
+        if (/[*\/]/.test(op)) {
+          if (op === '*') {
+            work.push("" + n1 * n2);
+          } else {
+            work.push("" + n2 / n1);
+          }
+        } else {
+          console.log("erreur")
+          work.push(n1.toString());
+          work.push(op);
+          work.push(n2.toString());
+        }
+      }
+    }
+    return work.reverse();
+  }
+
+  formatterCalcul(){
+    this.affichageCalcul = this.calcul.join(" ");
+  }
+  formatterResultat(){
+
+  }
+
+  checkCalcul() : boolean{
+    const n = Math.floor(this.calcul.length / 2);
+    if(this.calcul.length == n*2+1){
+      return true;
+    }else{
+      return false;
+    }
+  }
 }
