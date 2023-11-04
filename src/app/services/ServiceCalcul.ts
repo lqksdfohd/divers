8100f0935cd46009f87a3b9497974a5065c82394
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-11-04 15:12:04 +0100

    fini le service de calcul

--- src/app/services/ServiceCalcul.ts
+++ src/app/services/ServiceCalcul.ts
@@ -1,32 +1,36 @@
-export class ServiceCalcul{
-    formule:string[] = [];
+import { Injectable, Output } from "@angular/core";
 
-    isFormuleVide(){
+
+@Injectable({providedIn: "root"})
+export class ServiceCalcul {
+    formule: string[] = [];
+
+    isFormuleVide() {
         return this.formule.length === 0;
     }
 
-    push(nbr:string):void{
+    push(nbr: string): void {
         const regxChiffres = /[0-9]+/;
         const regxOperations = /[+-/*]+/;
         let precedent = this.formule.pop() as string;
         //si formule est vide
-        if(!precedent){
+        if (!precedent) {
             //si n est un chiffre
-            if(!regxOperations.test(nbr)){
+            if (!regxOperations.test(nbr)) {
                 //on l'append à formule
                 this.formule.push(nbr);
             }
         }
         //si le dernier élément de formule est un nombre et l'input n un chiffre
-        else if(regxChiffres.test(precedent) && regxChiffres.test(nbr)){
+        else if (regxChiffres.test(precedent) && regxChiffres.test(nbr)) {
             //on concatene nombre et chiffre et on l'append à formule
             precedent += nbr;
             this.formule.push(precedent);
         }//si le dernier élément est une operation et l'input n aussi
-        else if(regxOperations.test(precedent) && regxOperations.test(nbr)){
+        else if (regxOperations.test(precedent) && regxOperations.test(nbr)) {
             //on remplace l'encienne operation par l'input
             this.formule.push(nbr);
-        }else{
+        } else {
             //si le dernier élément est un nombre et l'input n une opération alors ou 
             //l'inverse alors on append le dernier élément et ensuite l'input n.
             this.formule.push(precedent);
@@ -34,8 +38,81 @@ export class ServiceCalcul{
         }
     }
 
-    getNbrDeTermesDsFormule():number{
+    getNbrDeTermesDsFormule(): number {
         return this.formule.length;
+    }
+
+    calculer(): number {
+        if (this.isFormuleVide()) {
+            return 0;
+        } else if (this.getNbrDeTermesDsFormule() < 3) {
+            const temp: string = this.formule.shift() as string;
+            const output: number = Number.parseFloat(temp);
+            return output;
+        } else {
+            let temp: string[] = this.faireCalculsPrioritaires();
+            return Number.parseFloat(this.faireCalculNonPrioritaire(temp)[0]);
+        }
+    }
+
+    faireCalculsPrioritaires(): string[] {
+        let temp = this.formule.slice();
+        let output: string[] = [];
+
+        while (temp.length > 0) {
+            let gauche = temp.shift()!;
+            let op = temp.shift()!;
+            //tans qu'il y a des opérations
+            if (op != undefined) {
+                switch (op) {
+                    case "*":
+                        let termeDroiteMultiplication = Number.parseFloat(temp.shift()!);
+                        const resultatMultiplication = Number.parseFloat(gauche) * termeDroiteMultiplication;
+                        temp.unshift(resultatMultiplication.toString());
+                        break;
+                    case "/":
+                        let termeDroiteDivision = Number.parseFloat(temp.shift()!);
+                        const resultatDivision = Number.parseFloat(gauche) / termeDroiteDivision;
+                        temp.unshift(resultatDivision.toString());
+                        break;
+                    // si opération non prioritaire on l'ajoute 
+                    //à l'output sans modifier l'opération.
+                    default:
+                        output.push(gauche);
+                        output.push(op);
+
+                }
+            } else {
+                output.push(gauche);
+            }
+        }
+        return output;
+    }
+
+    faireCalculNonPrioritaire(input: string[]): string[] {
+        while (input.length > 0) {
+            let droite: string = input.pop()!;
+            let op: string = input.pop()!;
+            if (op != undefined) {
+                switch (op) {
+                    case "+":
+                        let NgaucheAddition = Number.parseFloat(input.pop()!);
+                        let NdroiteAddition = Number.parseFloat(droite);
+                        input.push((NgaucheAddition + NdroiteAddition).toString());
+                        break;
+                    case "+":
+                        let NgaucheSoustraction = Number.parseFloat(input.pop()!);
+                        let NdroiteSoustraction = Number.parseFloat(droite);
+                        input.push((NgaucheSoustraction - NdroiteSoustraction).toString());
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                input.push(droite);
+            }
+        }
+        return input;
     }
 
 }
--- src/app/services/calcul.service.spec.ts
+++ src/app/services/calcul.service.spec.ts
@@ -41,6 +41,104 @@ describe("test push:", () =>{
         serviceCalcul.push("-");
         serviceCalcul.push("8");
         expect(serviceCalcul.getNbrDeTermesDsFormule()).toBe(3);
+    })//cas input garbage
+})
+
+describe("test calcul: ", () =>{
+    let serviceCalcul:ServiceCalcul;
+    beforeEach(() => { serviceCalcul = new ServiceCalcul();})
+    it("cas formule vide", () => {
+        const resultat = serviceCalcul.calculer();
+        expect(resultat).toBe(0);
     })
-    
+    it("cas nbr sans opération", () => {
+        serviceCalcul.push("9");
+        const resultat = serviceCalcul.calculer();
+        expect(resultat).toBe(9);
+    })
+    it("cas avec opération mais calcul incomplet", () => {
+        serviceCalcul.push("9");
+        serviceCalcul.push("+");
+        const resultat = serviceCalcul.calculer();
+        expect(resultat).toBe(9)
+    })
+})
+
+describe("test calcul prioritaire: ",() => {
+    let serviceCalcul:ServiceCalcul;
+    beforeEach( () => { serviceCalcul = new ServiceCalcul(); })
+    it("cas formule contient que des opérations non prioritaires", () =>{
+        serviceCalcul.push("9");
+        serviceCalcul.push("+");
+        serviceCalcul.push("9");
+        const resultat = serviceCalcul.faireCalculsPrioritaires();
+        expect(resultat.length).toBe(3);
+    })
+    it("cas formule contient plusieurs opérations non prioritaires", () => {
+        serviceCalcul.push("9");
+        serviceCalcul.push("+");
+        serviceCalcul.push("9");
+        serviceCalcul.push("+");
+        serviceCalcul.push("9");
+        const resultat = serviceCalcul.faireCalculsPrioritaires();
+        expect(resultat.length).toBe(5);
+    })
+    it("cas un seul calcul prioritaire", () => {
+        serviceCalcul.push("9");
+        serviceCalcul.push("*");
+        serviceCalcul.push("9");
+        const resultat = serviceCalcul.faireCalculsPrioritaires();
+        expect(resultat[0]).toBe("81");
+    })
+
+    it("cas plusieurs opérations prioritaires multiplication", () => {
+        serviceCalcul.push("9");
+        serviceCalcul.push("*");
+        serviceCalcul.push("9");
+        serviceCalcul.push("*");
+        serviceCalcul.push("2");
+        const resultat = serviceCalcul.faireCalculsPrioritaires();
+        expect(resultat[0]).toBe("162");
+    })
+    it("cas une opération non prioritaire avant une opération prioritaire", () => {
+        serviceCalcul.push("9");
+        serviceCalcul.push("+");
+        serviceCalcul.push("9");
+        serviceCalcul.push("*");
+        serviceCalcul.push("2");
+        const resultat = serviceCalcul.faireCalculsPrioritaires();
+        expect(resultat).toEqual(["9","+","18"]);
+    })
+    it("cas opération prioritaire avant opération non prioritaire", () =>{
+        serviceCalcul.push("9");
+        serviceCalcul.push("*");
+        serviceCalcul.push("2");
+        serviceCalcul.push("+");
+        serviceCalcul.push("9");
+        const resultat = serviceCalcul.faireCalculsPrioritaires();
+        console.log("__"+resultat);
+        expect(resultat).toEqual(["18", "+", "9"]);
+    })
+    it("cas plusieurs opérations prioritaires division", () => {
+        serviceCalcul.push("4");
+        serviceCalcul.push("/");
+        serviceCalcul.push("2");
+        serviceCalcul.push("/");
+        serviceCalcul.push("2");
+        const resultat = serviceCalcul.faireCalculsPrioritaires();
+        console.log(resultat);
+        expect(resultat).toEqual(["1"]);
+    })
+})
+
+describe("test calcul non prioritaire", () => {
+    let serviceCalcul:ServiceCalcul;
+    beforeEach(() => { serviceCalcul = new ServiceCalcul();})
+    it("cas une opération", () => {
+        serviceCalcul.push("9");
+        serviceCalcul.push("+");
+        serviceCalcul.push("9");
+        const resultat = serviceCalcul.faireCalculNonPrioritaire(serviceCalcul.formule);
+        expect(resultat).toEqual(["18"]);
+    })
 })
